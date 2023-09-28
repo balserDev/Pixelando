2ba944556298ded954926ca9703caa97158da197
mateo <mateovalencia02@gmail.com>
2023-09-28 13:46:53 -0700

    First commit (project base) 🎉

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = 3000;
+const app = express();
+var frameUnits = 1000;
+var height = 1;
+var width = 1;
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(port, (req, res)=>{
+  console.log(`The server has started on the port ${port}`)
+})
+
+app.get("/", (req, res)=>{
+  res.render("main.ejs");
+})
+
+app.get("/canvas", (req, res)=>{
+  console.log(height + " " + width)
+  res.status(200).send({width:width, height:height})
+})
+
+app.post("/resolution", (req, res)=>{
+  let frameSize = req.body
+
+  height = frameSize.height;
+  width = frameSize.width;
+  
+  frameUnits = height * width;
+  res.render("main.ejs", {units:frameUnits, width:width, height:height});
+})
+
+
+
---
+++ public/scripts/colorPicker.js
@@ -0,0 +1,42 @@
+
+function getCanvas(){
+  const url = "http://localhost:3000/canvas"
+  fetch(url)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+
+    $(".container").css("grid-template-rows", "repeat("+data.height+", 1fr)")
+    $(".container").css("grid-template-columns", "repeat("+data.width+", 1fr)")
+  })
+}
+
+getCanvas();
+
+let colorpicker = $(".colorpicker");
+
+$(".block").click(function(){
+  console.log("hit");
+  let color = colorpicker.val();
+  $(this).css("background-color", color)
+})
+
+// $(".container").css("grid-template-rows", repeat(height, "1fr"))
+// $(".container").css("grid-template-columns", repeat(width, "1fr"))
+
+$("#download").click(function(){
+  const screenshot = document.querySelector("#capture");
+  html2canvas(screenshot).then((canvas)=>{
+    const image = canvas.toDataURL("image/png");
+    let anchor = document.createElement('a');
+    anchor.setAttribute("href", image);
+    anchor.setAttribute("download", "my-art.png");
+    anchor.click();
+    anchor.remove()
+
+  })
+})
+    
+
+         
