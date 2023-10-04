3c62ff1cfd97ccb127399bfa65c3918c129a5316
Mateo Valencia <mateovalencia02@gmail.com>
2023-10-04 16:56:52 -0600

    added ui and canvas functionality :sparkles:

--- public/scripts/colorPicker.js
+++ public/scripts/colorPicker.js
@@ -13,9 +13,20 @@ function getCanvas(){
     return response.json();
   })
   .then(function(data){
-
-    $(".container").css("grid-template-rows", "repeat("+data.height+", 1fr)")
-    $(".container").css("grid-template-columns", "repeat("+data.width+", 1fr)")
+    if(data.width <= 100 && data.width > 0 && data.height <= 100 && data.height > 0){
+      let containerWidth = data.width * 10;
+      let containereHeight = data.height * 10;
+      $(".container").css("grid-template-rows", "repeat("+data.height+", 1fr)")
+      $(".container").css("grid-template-columns", "repeat("+data.width+", 1fr)")
+      $(".container").css("width", containerWidth);
+      $(".container").css("height", containereHeight);
+    }else if(data.width && data.height > 100){
+      alert("Max canvas size is 100")
+    }
+    else(
+      alert("min canvas size is 100")
+    )
+   
   })
 }
 
@@ -23,12 +34,45 @@ getCanvas();
 
 let colorpicker = $(".colorpicker");
 
-$(".block").click(function(){
-  console.log("hit");
+$(".container").mousedown(function(){
+console.log("entering")
   let color = colorpicker.val();
+  $(".block").mouseenter(function(){
   $(this).css("background-color", color)
+  })
+})
+$(".container").mouseup(function(){
+  $(".block").off("mouseenter");
+  console.log("exiting")
+})
+  
+$(".container").mouseleave(function(){
+  $(".block").off("mouseenter");
 })
 
+
+$(".block").click(function(){
+ // console.log("hit");
+  let color = colorpicker.val();
+  $(this).css("background-color", color)
+  // $(".block").mouseenter(function(){
+  //   $(this).css("background-color", color)
+  // })
+})
+
+
+$("#clear-button").click(function(){
+  var result = confirm("you are about to clear the canvas")
+  if(result){
+    $(".block").css("background-color", "white")
+  }else{
+    event.preventDefault();
+  }
+  
+})
+// $("body").mouseup(function(){
+//   $()
+// })
 
 $("#download").click(function(){
   const screenshot = document.querySelector("#capture");
